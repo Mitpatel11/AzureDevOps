171e5477aed88a03f494bb71d451853eeda2bf25
Mitpatel11 <87959696+Mitpatel11@users.noreply.github.com>
2024-11-29 10:18:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ data "aws_key_pair" "existing_key" {
 
 # EC2 Instance using the key pair and security group
 resource "aws_instance" "vm" {
-  ami           = "ami-0917d3c16c89e5dc3"
+  ami           = "ami-083c5e41e82da26fb"
   instance_type = "a1.medium"
   key_name      = data.aws_key_pair.existing_key.key_name
 
